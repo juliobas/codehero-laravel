4a9cc7c97bee372b5bf655caada92997c157a929
juliobas <juliobas@gmail.com>
2014-07-20 17:28:42 -0430

    parte 1 del proyecto

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,22 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+Route::get('usuarios', array('uses' => 'UsuariosController@mostrarUsuarios'));
+
+Route::get('tienda', function()
+{   
+    // Con la funcion with() podemos traer todos los vendedores 
+    // con sus respectivos productos. Esta funcion recibe como parametro 
+    // alguna relacion que tenga el modelo al que se este llamando y 
+    // la incluye en los resultados que devuelve el get()
+    $vendedores = Vendedor::with('productos')->get();
+    return View::make('inicio', array('vendedores'=> $vendedores));
+});
+ 
+Route::get('vendedor', 'VendedorController@mostrarVendedores');
+ 
+Route::post('vendedor', 'VendedorController@crearVendedor');
+ 
+Route::get('producto', 'ProductoController@mostrarProductos');
+ 
+Route::post('producto', 'ProductoController@crearProducto');
